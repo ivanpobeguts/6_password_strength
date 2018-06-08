d226dd0d1ffde1dda0449bf409d4dc8b0f837e47
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-08 11:09:28 +0300

    + README + blacklist check

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This script calculates the strength of your password. Checking parameters:
+* Password length
+* Digits
+* Upper- and lowercase letters
+* Special symbols (**!@#$%^&\*()[]{}**)
+* Is password in blacklist
+
+You can provide your own passwords blacklist as a script parameter. For example, download from here:
+[10-million-password-list-top-100](https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-100.txt)
+
+# How to
+
+Script requires Python 3.5. Example of launch on Linux/Windows:
+
+```bash
+
+$ python password_strength.py blacklist.txt
+
+Password:
+Password strength is: 8
+
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,5 +1,10 @@
 import getpass
-import re
+import argparse
+
+
+def load_data(filepath):
+    with open(filepath, 'r') as file:
+        return file.read()
 
 
 def check_digits(pwd):
@@ -30,18 +35,32 @@ def check_length(pwd):
         points += 2
     return points
 
-def check_pwd_in_blacklist(pwd):
-    pass
+
+def check_pwd_in_blacklist(pwd, blacklist):
+    return pwd in blacklist
 
 
 def get_password_strength(pwd):
-    strength = 0
+    strength = 2
     if check_digits(pwd): strength += 1
     if check_letters(pwd): strength += 1
     if check_case_sensitivity(pwd): strength += 2
     if check_special_symbols(pwd): strength += 2
+    if check_pwd_in_blacklist(pwd, blacklist): strength -= 1
     return strength + check_length(pwd)
 
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='Path to blacklist file')
+    return parser
+
+
 if __name__ == '__main__':
-    pwd = getpass.getpass('Password:')
-    print(get_password_strength(pwd))
+    try:
+        args = get_parser().parse_args()
+        blacklist = load_data(args.filepath)
+        pwd = getpass.getpass('Password:')
+        print('Password strength is:', get_password_strength(pwd))
+    except FileNotFoundError:
+        print('File not found.')
