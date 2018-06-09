4f67a0e75785412b97d74038e805d6a09c71d5f5
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-09 12:56:01 +0300

    - global variables read -> read.splitlines()

--- password_strength.py
+++ password_strength.py
@@ -1,53 +1,62 @@
 import getpass
 import argparse
+import string
+import os
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as file:
-        return file.read()
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as file:
+            return file.read().splitlines()
+    else:
+        return []
 
 
-def check_digits(pwd):
-    return any(symbol.isdigit() for symbol in pwd)
+def check_digits(password):
+    return any(symbol.isdigit() for symbol in password)
 
 
-def check_letters(pwd):
-    return any(symbol.isalpha() for symbol in pwd)
+def check_letters(password):
+    return any(symbol.isalpha() for symbol in password)
 
 
-def check_case_sensitivity(pwd):
-    lower_letter = any(symbol.islower() for symbol in pwd)
-    upper_letter = any(symbol.isupper() for symbol in pwd)
+def check_case_sensitivity(password):
+    lower_letter = any(symbol.islower() for symbol in password)
+    upper_letter = any(symbol.isupper() for symbol in password)
     return lower_letter and upper_letter
 
 
-def check_special_symbols(pwd):
-    return any(symbol in "!@#$%^&*()[]{}" for symbol in pwd)
+def check_special_symbols(password):
+    return any(symbol in string.punctuation for symbol in password)
 
 
-def check_length(pwd):
+def count_length_points(password):
+    bad_length = 5
+    normal_length = 10
+    good_length = 15
     points = 0
-    if len(pwd) < 5:
+    if len(password) < bad_length:
         points -= 1
-    elif 10 < len(pwd) <= 15:
+    elif normal_length < len(password) <= good_length:
         points += 1
-    elif len(pwd) > 15:
+    elif len(password) > good_length:
         points += 2
     return points
 
 
-def check_pwd_in_blacklist(pwd, blacklist):
-    return pwd in blacklist
+def check_password_in_blacklist(password, blacklist):
+    return password in blacklist
 
 
-def get_password_strength(pwd):
-    strength = 2
-    if check_digits(pwd): strength += 1
-    if check_letters(pwd): strength += 1
-    if check_case_sensitivity(pwd): strength += 2
-    if check_special_symbols(pwd): strength += 2
-    if check_pwd_in_blacklist(pwd, blacklist): strength -= 1
-    return strength + check_length(pwd)
+def get_password_strength(password, blacklist):
+    init_strength = 2
+    return (init_strength
+            + check_digits(password)
+            + check_letters(password)
+            + 2 * check_case_sensitivity(password)
+            + 2 * check_special_symbols(password)
+            - check_password_in_blacklist(password, blacklist)
+            + count_length_points(password))
 
 
 def get_parser():
@@ -57,10 +66,9 @@ def get_parser():
 
 
 if __name__ == '__main__':
-    try:
-        args = get_parser().parse_args()
-        blacklist = load_data(args.filepath)
-        pwd = getpass.getpass('Password:')
-        print('Password strength is:', get_password_strength(pwd))
-    except FileNotFoundError:
-        print('File not found.')
+    args = get_parser().parse_args()
+    blacklist = load_data(args.filepath)
+    if not blacklist:
+        print('Warning: your blacklist is empty')
+    password = getpass.getpass('Password:')
+    print('Password strength is:', get_password_strength(password, blacklist))
